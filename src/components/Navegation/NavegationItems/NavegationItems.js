048f845a6009763446cd175a67d7f0a5c53dc75a
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-11-27 12:14:13 -0300

    added homelink to toolbar

--- src/components/Navegation/NavegationItems/NavegationItems.js
+++ src/components/Navegation/NavegationItems/NavegationItems.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import classes from './NavegationItems.module.css';
-import NavegationItem from './NavegationItem/NavegationItem';
-import {VscArrowRight} from 'react-icons/vsc'
+import React from "react";
+import classes from "./NavegationItems.module.css";
+import NavegationItem from "./NavegationItem/NavegationItem";
+import { VscArrowRight } from "react-icons/vsc";
 
-const NavegationItems = (props)=>{
-    return(
-        <ul className={classes.NavegationItems}>
-        <NavegationItem destination='/' fontSize='20px'>Account</NavegationItem>
-        <NavegationItem destination='/'  fontSize='20px'>Log Out <VscArrowRight style={{ verticalAlign: "middle" }}/> </NavegationItem>
-        </ul>
-    )
-}
+const NavegationItems = (props) => {
+  return (
+    <ul className={classes.NavegationItems}>
+      <NavegationItem destination="/" fontSize="20px">
+        Home
+      </NavegationItem>
+      <NavegationItem destination="/" fontSize="20px">
+        Account
+      </NavegationItem>
+      <NavegationItem destination="/" fontSize="20px">
+        Log Out <VscArrowRight style={{ verticalAlign: "middle" }} />{" "}
+      </NavegationItem>
+    </ul>
+  );
+};
 
 export default NavegationItems;
