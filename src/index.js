5e49f66f5bcb4d701613213a0b562b944687aa8d
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-12-03 16:15:43 -0300

    git pages config

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ sagaMiddleware.run(watchAuth);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
