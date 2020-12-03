702f90012a3f2e8bdbb1b98d0244b9fdf54b8d7e
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-12-03 15:01:21 -0300

    added filter functionality

--- src/containers/Games/Games.js
+++ src/containers/Games/Games.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FetchGameType } from "../../store/actions/index";
 import { connect } from "react-redux";
 import classes from "./Games.module.css";
@@ -8,26 +8,41 @@ import UnStyledLink from "../../components/Navegation/UnStyledLink/UnStyledLink"
 import GameTypes from "../../components/Games/GameTypes";
 import GameCards from "../../components/Games/GameCards";
 const Games = (props) => {
-  const {OnInitGames, types } = props;
-  const [filters,SetFilters] = useState([])
+  const { OnInitGames, types } = props;
+  const [filters, SetFilters] = useState([]);
+  const [filteredGame, setFilteredGames] = useState(props.prevGames);
   useEffect(() => {
     OnInitGames();
-    console.log("my prev Games ",props.prevGames)
   }, [OnInitGames]);
-  const handleFilter = (fil)=>{
-      const newFilters = [...filters,fil]
-      SetFilters(newFilters)
-      console.log('myFilters: ' + filters)
-  }
+  const handleFilter = (fil) => {
+    let newFilters = [...filters];
+    let filteredContent = [...filteredGame];
+    if (newFilters.indexOf(fil) !== -1) {
+      newFilters = filters.filter((filItem) => filItem !== fil);
+    } else {
+      newFilters = [...newFilters, fil];
+    }
+    newFilters.length === 0
+      ? (filteredContent = props.prevGames)
+      : (filteredContent = props.prevGames.filter((game) =>
+          newFilters.includes(game.name)
+        ));
+    SetFilters(newFilters);
+    setFilteredGames(filteredContent);
+  };
   return (
     <div className={classes.Games}>
       <div className={classes.GameContent}>
-      <div className={classes.GamesHeader}>
-        <h3>RECENT GAMES</h3>
-        <p>Filters</p>
-        <GameTypes types={types} clickHandler={handleFilter} activeArray={filters}/>
-      </div>
-      <GameCards cardObjs={props.prevGames}/>
+        <div className={classes.GamesHeader}>
+          <h3>RECENT GAMES</h3>
+          <p>Filters</p>
+          <GameTypes
+            types={types}
+            clickHandler={handleFilter}
+            activeArray={filters}
+          />
+        </div>
+        <GameCards cardObjs={filteredGame} />
       </div>
 
       <StyledButton colored size="24px">
@@ -41,7 +56,7 @@ const Games = (props) => {
 const mapStateToProps = (state) => {
   return {
     types: state.games.types,
-    prevGames : state.games.prevGames
+    prevGames: state.games.prevGames,
   };
 };
 const mapDispatchToProps = (dispatch) => {
